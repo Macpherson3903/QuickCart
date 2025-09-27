8a64a9121c7e421c98ec09041eb156d92f5cb9d7
Macpherson3903 <109850340+Macpherson3903@users.noreply.github.com>
2025-09-27 18:19:38 +0100

    Update user.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
+    _id: { type: String, required: true }, // Clerk User ID
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
-    cartItems: { type: Object, default: [] },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
-}, { minimize: false })
+    cartItems: { type: Object, default: [] }, // can later change to [CartItemSchema]
+    isAdmin: { type: Boolean, default: false }, // no need required:true
+}, { minimize: false });
 
-const User = mongoose.models.user || mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
